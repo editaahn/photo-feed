3e3d51872ebf6402989ba5d483305ededc2a757b
editaahn <41330862+editaahn@users.noreply.github.com>
2021-01-25 21:08:34 +0900

    CardList : state 정의 및 request 코드

--- src/components/list/CardList.js
+++ src/components/list/CardList.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EmptyCardList from "./EmptyCardList";
 import ExistingCardList from "./ExistingCardList";
+import { requestCards } from "../../libraries/request";
 
 const CardList = () => {
-  const cardList = [];
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(undefined);
+
+  useEffect(() => {
+    setLoading(true);
+    requestCards().then(({ data }) => {
+      setCards(data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    // 로딩 중일 때 처리
+  }
+
   return (
     <section>
-      {cardList.length ? <ExistingCardList list={cardList} /> : <EmptyCardList />}
+      {cards.length ? <ExistingCardList list={cards} /> : <EmptyCardList />}
     </section>
   );
 };
