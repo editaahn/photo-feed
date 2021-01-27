f5e47d404f567eebe4888dbd2a2892c3f44556ee
editaahn <41330862+editaahn@users.noreply.github.com>
2021-01-27 22:37:23 +0900

    Notice : setNotice를 통해서만 렌더링하도록 구조 변경

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -7,34 +7,34 @@ import ListContext from "../../contexts/list";
 import "../../styles/components/card.scss";
 
 const Feed = () => {
-  const { cards, loading } = useContext(ListContext).state;
+  const { cards } = useContext(ListContext).state;
+  const { scraps, toastMessage } = useContext(ScrapContext).state;
+
   const { updateCards, setLoading } = useContext(ListContext).actions;
-  const { scraps, isFiltered, toastMessage } = useContext(ScrapContext).state;
 
   const [notice, setNotice] = useState("");
 
   useEffect(() => {
     setLoading(true);
+    setNotice("컨텐츠를 불러오는 중입니다.");
     updateCards()
-      .then(({ message }) => message && setNotice(message))
+      .then(({ message }) => setNotice(message || ""))
       .then(() => setLoading(false));
   }, []);
 
   return (
-    <>
+    <section className="cardContainer">
       {cards.length > 0 && (
         <List
           list={cards.map((card) => {
             card.scrapped = scraps.includes(card.id);
             return card;
           })}
-          setNotice={setNotice}
         />
       )}
-      {loading && <Notice text="이미지를 불러오는 중입니다." />}
-      {notice && !isFiltered && <Notice text={notice} />}
+      {notice && <Notice text={notice} />}
       {toastMessage && <Toast name="scrappingToast" text={toastMessage} />}
-    </>
+    </section>
   );
 };
 
--- src/components/feed/List.js
+++ src/components/feed/List.js
@@ -6,19 +6,20 @@ const List = ({ list, setNotice }) => {
   const { isFiltered } = useContext(ScrapContext).state;
   const renderedList = isFiltered ? list.filter((item) => item.scrapped) : list;
 
+  !renderedList.length &&
+    isFiltered &&
+    setNotice("스크랩된 컨텐츠가 없습니다.");
+
   return (
-    <section className="cardContainer">
-      <ul className="cardList">
-        {renderedList.map((item, i) => (
-          <Card
-            key={item.id}
-            data={item}
-            isLastOne={i === renderedList.length - 1}
-            setNotice={setNotice}
-          />
-        ))}
-      </ul>
-    </section>
+    <ul className="cardList">
+      {renderedList.map((item, i) => (
+        <Card
+          key={item.id}
+          data={item}
+          isLastOne={i === renderedList.length - 1}
+        />
+      ))}
+    </ul>
   );
 };
 
