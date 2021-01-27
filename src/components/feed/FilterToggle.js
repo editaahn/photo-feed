84cff84ab47f862c8448be12b646ccc37c79e1a1
editaahn <41330862+editaahn@users.noreply.github.com>
2021-01-28 00:48:46 +0900

    함수 props 전달 방식 변경 : 인라인 함수 -> 로컬 함수
    성능향상

--- src/components/feed/FilterToggle.js
+++ src/components/feed/FilterToggle.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext } from "react";
 import Toggle from "../modules/Toggle";
 import ScrapContext from "../../contexts/scrap";
 import NoticeContext from "../../contexts/notice";
@@ -7,15 +7,17 @@ const FilterToggle = () => {
   const { isFiltered, setIsFiltered } = useContext(ScrapContext);
   const { setNotice } = useContext(NoticeContext);
 
+  const onChange = () => {
+    setIsFiltered(!isFiltered);
+    setNotice("");
+  };
+
   return (
     <section className="filterContainer">
       <Toggle
         toggleName="filter"
         checkedState={isFiltered}
-        onChangeState={() => {
-          setIsFiltered(!isFiltered);
-          setNotice("");
-        }}
+        onChangeState={onChange}
       >
         <label>스크랩한 것만 보기</label>
       </Toggle>
